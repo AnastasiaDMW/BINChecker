11524aed7fe356151995c1969a5f187781916857
AnastasiaDMW <AnastasiaZVV1@gmail.com>
2025-01-17 20:25:49 +0500

    Removed unnecessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,10 +57,6 @@ dependencies {
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
-//    //Lifecycle
-//    implementation(libs.androidx.lifecycle.livedata.ktx)
-//    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-//    implementation(libs.androidx.lifecycle.common.java8)
     //Retrofit
     implementation(libs.com.squareup.retrofit2)
     implementation(libs.com.squareup.converter.gson)
